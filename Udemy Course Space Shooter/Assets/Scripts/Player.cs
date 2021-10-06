c67d60f1e74f23a0315d662282daad13934d9025
Sung Kim <ksungha66@gmail.com>
2021-10-05 18:21:44 -0700

    Adding player bounds for the y and x axis

--- Udemy Course Space Shooter/Assets/Scripts/Player.cs
+++ Udemy Course Space Shooter/Assets/Scripts/Player.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
@@ -8,7 +11,6 @@ public class Player : MonoBehaviour
     private float _speed = 5f;
     void Start()
     {
-        //take current position = new position (0,0,0)
         transform.position = new Vector3(0, 0, 0);
     }
     void Update()
@@ -18,5 +20,32 @@ public class Player : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        //adding player bounds
+        //if player position on y is > 0
+        //y position = 0
+        //else if y pos is < -3.8f
+        //y pos = -3.8f
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+        //if player on the x > 11
+        //x pos -11
+        //else if player on the x is less than -11
+        //x pos = 11
+        if (transform.position.x >= 10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -10)
+        {
+            transform.position = new Vector3(10, transform.position.y, 0);
+        }
     }
 }
