3eb6e862ff8fd7979f2e3c97019d84f3ac33eb43
Sung Kim <ksungha66@gmail.com>
2022-05-28 11:40:04 -0700

    Adding logic for spawn manager
    Infinitely spawns enemies at a rate of 5 secs, placed into a container to clean up objects

---
+++ Udemy Course Space Shooter/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    float waitTime = 5f;
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
